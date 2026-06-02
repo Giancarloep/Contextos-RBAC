c99e2bdd3d94ad4c1f54be25f9c448b4f670fc74
Giancarloep <giancarlo_echeverria@unitec.edu>
2026-06-02 15:07:55 -0600

    AppNavigatorFix, starting logging

--- TareaContextRBAC/src/navigation/AppNavigator.tsx
+++ TareaContextRBAC/src/navigation/AppNavigator.tsx
@@ -1,18 +1,24 @@
+import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import HomeScreen from "../screens/HomeScreen";
 import SettingsScreen from "../screens/SettingScreen";
+import LoginScreen from "../screens/LoginScreen";
 import { useAuth } from "../context/AuthContext";
 
 type TabsParamList = {
   Home: undefined;
   Settings: undefined;
+  Login: undefined;
 };
 
 const Tab = createBottomTabNavigator<TabsParamList>();
+
 export type { TabsParamList };
+
 export default function TabNavigator() {
   const { userRole } = useAuth();
+
   return (
     <Tab.Navigator
       initialRouteName={userRole === "admin" ? "Settings" : "Home"}
@@ -26,11 +32,22 @@ export default function TabNavigator() {
         headerTintColor: "#fff"
       }}
     >
+        <Tab.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{
+          title: "Login",
+          headerShown: true,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="desktop" size={size} color={color} />
+          ),
+        }}
+      />
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          title: "Inicio",
+          title: "Home",
           headerShown: true,
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="home" size={size} color={color} />
